9a2258936a8122a84541870e31796168cc6286c4
rida-el <rida.el99@gmail.com>
2022-08-02 17:05:14 +0100

    add how it work section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Hero from "./componants/Hero/Hero";
 import Btc from "./componants/Btc/Btc";
+import How from "./componants/How/How";
 
 function App() {
   return (
     <div className="App">
       <Hero></Hero>
       <Btc></Btc>
+      <How></How>
     </div>
   );
 }
--- src/componants/How/How.jsx
+++ src/componants/How/How.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
-import "How.css";
+import "./How.css";
+import data from "../data/How.json";
 
 function How() {
-  return <div></div>;
+  return (
+    <div className="howitwork">
+      <h1 className="title">How it Work</h1>
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eos
+        doloribus, maxime mollitia magnam, iure pariatur libero repellat ipsa
+        nihil adipisci ab impedit maiores quia aliquid veniam enim sit
+        consequatur!
+      </p>
+      <div className="cards">
+        {data.map((item, i) => (
+          <div className="cardfor">
+            <img src={item.image} />
+            <div className="contenthow">
+              <h4>{item.step} </h4>
+              <h3>{item.title} </h3>
+              <p>{item.paragraph} </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default How;
