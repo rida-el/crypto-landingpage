7cd6012cb327cd8a035d2ce8fd7eb98437268171
rida-el <rida.el99@gmail.com>
2022-08-02 03:57:10 +0100

    add card for secend section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Hero from './componants/Hero/Hero';
+import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./componants/Hero/Hero";
+import Btc from "./componants/Btc/Btc";
 
 function App() {
   return (
     <div className="App">
       <Hero></Hero>
+      <Btc></Btc>
     </div>
   );
 }
---
+++ src/componants/Btc/Btc.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./Btc.css";
+
+function Btc() {
+  return <div className="section2">
+    <div className="firstcard">
+        <div className="four">
+            iii
+        </div>
+        <div className="four">
+            hhh
+        </div>
+        <div className="four">
+            hhhh
+        </div>
+        <div className="four">
+            jjj
+        </div>
+    </div>
+  </div>;
+}
+export default Btc;
